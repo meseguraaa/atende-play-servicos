644650397fed7ddafb7a5f99e3c95ff59d70da32
meseguraaa <brunoleal2005@yahoo.com.br>
2025-11-04 22:00:41 -0300

    Configurando a home - parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image';
-
 export default function Home() {
     return (
-        <div>
-            <h1 className="text-blue-700">Welcome to My Next.js App</h1>
+        <div className="bg-background-primary p-6">
+            <div className="flex items-center justify-between md:m-8">
+                <div>
+                    <h1 className="text-title text-content-primary mb-2">
+                        Sua Agenda
+                    </h1>
+                    <p className="text-paragrahp-small text-content-secondary">
+                        Aqui você pode ver todos os cliente e serviços agendados
+                        para hoje
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
