2353ab13085fca0d617170532a8653c86158ff80
meseguraaa <brunoleal2005@yahoo.com.br>
2025-11-04 21:46:43 -0300

    Configurando o global.css e instanlando o shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <div>
-            <h1>Welcome to My Next.js App</h1>
+            <h1 className="text-blue-700">Welcome to My Next.js App</h1>
         </div>
     );
 }
