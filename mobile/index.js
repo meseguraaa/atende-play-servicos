8f404c7fbea1959790fef23454e9709c2933d4ff
meseguraaa <brunoleal2005@yahoo.com.br>
2026-02-07 19:34:29 -0300

    debug(mobile): log react and react-native versions on boot

--- mobile/index.js
+++ mobile/index.js
@@ -1,3 +1,23 @@
+// 🔍 Logs de diagnóstico no boot (antes de qualquer render)
+try {
+    // eslint-disable-next-line no-console
+    console.log('[BOOT] react version:', require('react/package.json').version);
+} catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('[BOOT] react version: <failed>', String(e));
+}
+
+try {
+    // eslint-disable-next-line no-console
+    console.log(
+        '[BOOT] react-native version:',
+        require('react-native/package.json').version
+    );
+} catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('[BOOT] react-native version: <failed>', String(e));
+}
+
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 import React from 'react';
